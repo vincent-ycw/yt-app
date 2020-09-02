4a1619abdfde43843ab2e0f98c805d2b994de308
vincent-ycw <vincent.ycw1@gmail.com>
2020-09-02 11:31:02 -0400

    updated label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
         }}
       >
         <div className="field">
-          <label>Search For A Video</label>
+          <label>Video Search</label>
           <input
             type="text"
             value={term}
